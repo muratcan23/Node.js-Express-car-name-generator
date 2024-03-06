e74b3c5c4f6d86d8b038bd762a09a0bce2dc1ce2
muratcan23 <muratal0606@gmail.com>
2024-03-06 04:59:28 +0300

    care name generatot with express

---
+++ index.js
@@ -0,0 +1,31 @@
+import bodyParser from "body-parser";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var carName = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function carNameGenerator(req, res, next) {
+  console.log(req.body);
+  carName = req.body["street"] + "-" + req.body["pet"];
+  next();
+}
+
+app.use(carNameGenerator);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your favorite car  : </h1> <h2>${carName} 🚗</h2>`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
